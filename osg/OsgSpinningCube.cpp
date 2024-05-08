e1f347da54418551e211ae1036fd9f4eebf39aef
mgbarro <m.gbarro@udc.es>
2024-05-08 16:40:10 +0200

    Añadido segundo cubo

--- osg/OsgSpinningCube.cpp
+++ osg/OsgSpinningCube.cpp
@@ -44,20 +44,32 @@ osg::Geometry* generateCube(float size) {
 int main() {
     // Generamos la geometría
     osg::ref_ptr<osg::Geometry> cubeGeometry = generateCube(1.0f);
+    osg::ref_ptr<osg::Geometry> secondCubeGeometry = generateCube(1.0f);
 
     // Creamos Geode para la geometría
     osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    osg::ref_ptr<osg::Geode> secondGeode = new osg::Geode;
     geode->addDrawable(cubeGeometry.get());
+    secondGeode-> addDrawable(secondCubeGeometry.get());
 
     // Creamos la matriz para la rotación
     osg::ref_ptr<osg::MatrixTransform> rotationTransform = new osg::MatrixTransform;
+    osg::ref_ptr<osg::MatrixTransform> secondRotationTransform = new osg::MatrixTransform;
     rotationTransform->addChild(geode.get());
+    secondRotationTransform->addChild(secondGeode.get());
+    
+    
+    osg::Matrix matrix = osg::Matrix::translate(osg::Vec3(-5.0, -1.0, 0.0)); // Posición del segundo cubo
+    rotationTransform->setMatrix(matrix);
+    osg::Matrix matrix2 = osg::Matrix::translate(osg::Vec3(5.0, 1.0, 0.0)); // Posición del segundo cubo
+    secondRotationTransform->setMatrix(matrix2);
     
     float moveSpeed = 0.1f;
 
     // Crear un nodo raíz de la escena
     osg::ref_ptr<osg::Group> root = new osg::Group;
     root->addChild(rotationTransform.get());
+    root->addChild(secondRotationTransform.get());
 
     // Visor con manipulación de cámara y escena
     osgViewer::Viewer viewer;
@@ -68,15 +80,22 @@ int main() {
     while (!viewer.done()) {
         double time = viewer.elapsedTime();
         float angle = osg::DegreesToRadians(static_cast<float>(time) * 45.0f);
-        osg::Matrix rotationMatrix;
+        osg::Matrix rotationMatrix, secondRotationMatrix;
         rotationMatrix.makeRotate(angle, osg::Vec3(1.0, 1.0, 0.0));
+        secondRotationMatrix.makeRotate(angle, osg::Vec3(-1.0, -1.0, 0.0));
         rotationTransform->setMatrix(rotationMatrix);
+        secondRotationTransform->setMatrix(secondRotationMatrix);
         
         osg::Vec3d translationVector(std::sin(time * moveSpeed) * 2.0, std::cos(time* moveSpeed) * 1.7, std::sin(time * 1.3) * std::cos(time * 1.5) * 2.0);
         osg::Matrix translationMatrix;
         translationMatrix.makeTranslate(translationVector);
         
+        osg::Vec3d secondTranslationVector(std::sin(time * moveSpeed) * -2.0, std::cos(time* moveSpeed) * -1.7, std::sin(time * 1.3) * std::cos(time * 1.5) * -2.0);
+        osg::Matrix secondTranslationMatrix;
+        secondTranslationMatrix.makeTranslate(secondTranslationVector);
+        
         rotationTransform->setMatrix(translationMatrix * rotationMatrix);
+        secondRotationTransform->setMatrix(secondTranslationMatrix * secondRotationMatrix);
         viewer.frame();
     }
 
